ad78607cea6a4d2fd332ff5dfd5a0f9eee5d2eaf
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-16 12:12:50 -0300

    Updates on stream pipeline and array type

--- src/com/swing/nicholas/Mainly.java
+++ src/com/swing/nicholas/Mainly.java
@@ -13,29 +13,29 @@ public class Mainly {
 
 	public static void main(String[] args) {
 
-		var varargs = new String[21];
+		var varargs = new Integer[21];
 		int i = 1;
 		while (i <= 20) {
-			varargs[i] = JOptionPane.showInputDialog(frame, "Ur text " + i + " of " + 4 + ":");
+			varargs[i] = Integer.valueOf(JOptionPane.showInputDialog(frame, "Number " + i + " of " + 20 ));
 			i++;
 		}
 
-		varargs = Arrays.asList(varargs).stream().map(x -> x == null ? "0" : x).collect(Collectors.toList())
-				.toArray(new String[0]);
+		varargs = Arrays.asList(varargs).stream().filter(x -> x != null).collect(Collectors.toList())
+				.toArray(new Integer[0]);
 
 		showResultDesc(varargs);
 		showResultAsc(varargs);
 	}
 
 	@SafeVarargs
-	private static void showResultDesc(String... varargs) {
+	private static void showResultDesc(Integer... varargs) {
 		Arrays.sort(varargs);
-		JOptionPane.showMessageDialog(frame, "Text in descending order " + Arrays.toString(varargs));
+		JOptionPane.showMessageDialog(frame, "Numbers in ascending order " + Arrays.toString(varargs));
 	}
 
 	@SafeVarargs
-	private static void showResultAsc(String... varargs) {
+	private static void showResultAsc(Integer... varargs) {
 		Arrays.sort(varargs, Comparator.reverseOrder());
-		JOptionPane.showMessageDialog(frame, "Text in ascending order " + Arrays.toString(varargs));
+		JOptionPane.showMessageDialog(frame, "Numbers in descending order " + Arrays.toString(varargs));
 	}
 }
